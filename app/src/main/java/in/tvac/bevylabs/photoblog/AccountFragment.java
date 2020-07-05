fede00284507094d3b16c37be826441fd505747d
wimanshaherath <wimanshah@gmail.com>
2020-07-06 00:11:59 +0530

    build changes

--- app/src/main/java/in/tvac/bevylabs/photoblog/AccountFragment.java
+++ app/src/main/java/in/tvac/bevylabs/photoblog/AccountFragment.java
@@ -3,9 +3,18 @@ package in.tvac.bevylabs.photoblog;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +22,10 @@ import android.view.ViewGroup;
  */
 public class AccountFragment extends Fragment {
 
+    private RecyclerView leaderboard_view;
+    public List<User> leaderBoardUsers;
+
+    private FirebaseFirestore firebaseFirestore;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -22,7 +35,18 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        leaderBoardUsers=new ArrayList<>();
         // Inflate the layout for this fragment
+        leaderboard_view=getActivity().findViewById(R.id.leaderboard_view);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseFirestore.collection("Users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+            }
+        });
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 
